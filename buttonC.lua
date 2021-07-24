5cb6cd556799b56393632be26952d3e6c1d08dbf
Sandor Kiraly <48065581+sndrkrly@users.noreply.github.com>
2021-07-24 08:08:14 +0200

    Add files via upload

--- buttonC.lua
+++ buttonC.lua
@@ -2,10 +2,15 @@ Button = {};
 
 local screenX, screenY = guiGetScreenSize();
 
-function Button.create(properties)
+function Button.create(id, properties)
      if type(properties) ~= "table" then
           properties = {};
+     elseif Button[id] then
+          print("the id is already exists!");
+          return;
      end
+
+     Button[id] = true;
  
      local function draw()
           local renderTarget = dxCreateRenderTarget(screenX, 70, true);
@@ -16,17 +21,17 @@ function Button.create(properties)
 
           dxSetRenderTarget(renderTarget);
           
-          dxDrawRectangle(0, 0, 250, 70, tocolor(200, 200, 200, 200));
-          dxDrawText(properties.text, 0, 0, 250, 70, tocolor(255, 255, 255, 255), 4, "default", "center", "center");
+          dxDrawRectangle(0, 0, 250, 250, tocolor(200, 200, 200, 200));
+          dxDrawText(properties.text, 0, 0, 250, 250, tocolor(255, 255, 255, 255), 4, "default", "center", "center");
      
           dxSetRenderTarget();
           dxDrawMaterialLine3D(
                properties.position.x,
                properties.position.y,
-               properties.position.z + (properties.size. / 2),
+               properties.position.z + (properties.size.y / 2),
                properties.position.x,
                properties.position.y,
-               properties.position.z - (properties.size. / 2),
+               properties.position.z - (properties.size.y / 2),
                renderTarget,
                properties.size.x,
                tocolor(255, 255, 255, 255),
